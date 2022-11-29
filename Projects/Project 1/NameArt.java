1c2666914c8a2062108e5c3082c75fdb97c94f04
Kyle Vinod <89536613+KyleVinod@users.noreply.github.com>
2022-11-29 17:18:25 -0500

    Create NameArt.java

---
+++ Projects/Project 1/NameArt.java
@@ -0,0 +1,39 @@
+/****************************************************************************
+* NameArt
+*****************************************************************************
+* Project description
+* I wrote my name on a sword blade
+* Kyle Vinod
+* 8/29/2021
+* CMSC 255 Section Project 1
+*****************************************************************************/
+
+public class NameArt
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Kyle Vinod");
+		System.out.println("Project 1 - Name in Code");
+		System.out.println("This project outputs my name on a sword blade");
+		System.out.println("August 29th, 2021");
+		System.out.println("CMSC 255 Section Project 1");
+		System.out.println("   ");
+		System.out.println("                 / \\");
+		System.out.println("                 \\ /");     
+		System.out.println("                  ||");  		
+		System.out.println("                  ||");
+		System.out.println("      / \\_________||________________________________");
+		System.out.println("     /              |   *  * *   * *   ***          \\");
+		System.out.println("    /               |   * *   * *  *   *             \\");
+		System.out.println("   /                |   **     *   *   ***            |");
+		System.out.println("   \\                |   * *    *   *   *             /");
+		System.out.println("    \\   __________  |___*_ *___*__ ***_***__________/");
+		System.out.println("     \\ /          ||");
+		System.out.println("                  ||");
+		System.out.println("                  ||");
+		System.out.println("                 / \\");
+		System.out.println("                 \\ /");
+		System.out.println();
+    }
+}
